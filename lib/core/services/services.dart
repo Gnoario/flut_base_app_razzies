0086b7f01ad4ae4ea79bb2ebbe5092740bc24371
Tadeu Jenuario <tadeujenuario@hotmail.com>
2024-04-28 02:09:11 -0300

    chore(infra): rename interceptors

--- lib/core/services/services.dart
+++ lib/core/services/services.dart
@@ -1,4 +1,4 @@
 export 'client_https/client_https.dart';
-export 'client_https/interceptors/errors_handle_interceptor.dart';
-export 'client_https/interceptors/headers_interceptor.dart';
+export 'client_https/interceptors/error_handle_interceptor.dart';
+export 'client_https/interceptors/headers_interceptors.dart';
 export 'client_https/interceptors/loggers_interceptors.dart';
