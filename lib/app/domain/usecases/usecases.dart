e4af92854d2889aa95ac9881ded0c5eb80e4a82c
Tadeu Jenuario <tadeujenuario@hotmail.com>
2024-04-29 12:00:49 -0300

    chore(winners): export usecase

--- lib/app/domain/usecases/usecases.dart
+++ lib/app/domain/usecases/usecases.dart
@@ -1,2 +1,4 @@
 export 'get_movies/get_movies_imp_usecase.dart';
 export 'get_movies/get_movies_usecase.dart';
+export 'get_years_with_multiple_winners/get_years_with_multiple_winners_imp_usecase.dart';
+export 'get_years_with_multiple_winners/get_years_with_multiple_winners_usecase.dart';
