c58c39eff6a6055e4ffad4da0b3c2d01b8d35a8b
Tadeu Jenuario <tadeujenuario@hotmail.com>
2024-04-29 14:53:51 -0300

    test(awards-interval): add mock

--- test/domain/usecases/get_max_min_win_interval_for_producers/get_max_min_win_interval_for_producers_usecase_test.dart
+++ test/domain/usecases/get_max_min_win_interval_for_producers/get_max_min_win_interval_for_producers_usecase_test.dart
@@ -1,7 +1,6 @@
 import 'package:flut_base_app_razzies/app/data/datasources/get_max_min_win_interval_for_producers/get_max_min_win_interval_for_producers_datasource.dart';
 import 'package:flut_base_app_razzies/app/data/repositories/get_max_min_win_interval_for_producers/get_max_min_win_interval_for_producers_imp_repository.dart';
 import 'package:flut_base_app_razzies/app/domain/models/exceptions/exceptions.dart';
-import 'package:flut_base_app_razzies/app/domain/models/exceptions/handled_exception.dart';
 import 'package:flut_base_app_razzies/app/domain/repositories/get_max_min_win_interval_for_producers/get_max_min_win_interval_for_producers_repository.dart';
 import 'package:flut_base_app_razzies/app/domain/usecases/get_max_min_win_interval_for_producers/get_max_min_win_interval_for_producers_imp_usecase.dart';
 import 'package:flut_base_app_razzies/app/domain/usecases/get_max_min_win_interval_for_producers/get_max_min_win_interval_for_producers_usecase.dart';
