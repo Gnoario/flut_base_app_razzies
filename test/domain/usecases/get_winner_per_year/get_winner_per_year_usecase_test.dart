231cde92980523251230cb827403e3d113d1e797
Tadeu Jenuario <tadeujenuario@hotmail.com>
2024-04-29 15:46:49 -0300

    test(year-winner): should return a MovieDto normally

--- test/domain/usecases/get_winner_per_year/get_winner_per_year_usecase_test.dart
+++ test/domain/usecases/get_winner_per_year/get_winner_per_year_usecase_test.dart
@@ -6,7 +6,9 @@ import 'package:flut_base_app_razzies/app/domain/usecases/get_winner_per_year/ge
 
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
 
+import '../../mocks/movie_mock.dart';
 import 'get_winner_per_year_usecase_test.mocks.dart';
 
 @GenerateMocks([GetWinnerPerYearDatasource])
@@ -14,10 +16,21 @@ void main() {
   late final GetWinnerPerYearDatasource datasource;
   late final GetWinnerPerYearRepository repository;
   late final GetWinnerPerYearUsecase usecase;
+  late final MovieMock mock;
 
-  setUp(() {
+  setUpAll(() {
     datasource = MockGetWinnerPerYearDatasource();
     repository = GetWinnerPerYearImpRepository(datasource);
     usecase = GetWinnerPerYearImpUsecase(repository);
+    mock = MovieMock();
+  });
+
+  group('Success Cases', () {
+    test('Should return a MovieDto normally', () async {
+      final movieMock = mock.createMovieMock();
+      when(datasource(year: 2021)).thenAnswer((_) async => movieMock);
+      final result = await usecase(year: 2021);
+      expect(result, movieMock);
+    });
   });
 }
