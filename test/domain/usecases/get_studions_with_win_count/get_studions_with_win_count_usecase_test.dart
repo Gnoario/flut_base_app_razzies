072fbdf3d48b5a26a6a277fbb07e9a83f9bdd3c0
Tadeu Jenuario <tadeujenuario@hotmail.com>
2024-04-29 12:46:02 -0300

    test(studios): should return a list of studio win count normally

--- test/domain/usecases/get_studions_with_win_count/get_studions_with_win_count_usecase_test.dart
+++ test/domain/usecases/get_studions_with_win_count/get_studions_with_win_count_usecase_test.dart
@@ -5,6 +5,7 @@ import 'package:flut_base_app_razzies/app/domain/usecases/get_studios_with_win_c
 import 'package:flut_base_app_razzies/app/domain/usecases/get_studios_with_win_count/get_studios_with_win_count_usecase.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
 
 import '../../mocks/studio_win_count_mock.dart';
 import 'get_studions_with_win_count_usecase_test.mocks.dart';
@@ -21,5 +22,20 @@ void main() {
     repository = GetStudionsWithWinCountImpRepository(datasource);
     usecase = GetStudionsWithWinCountImpUsecase(repository);
     studioWinCountMock = StudioWinCountMock();
+  });
+
+  group('Success Cases', () {
+    test('Should return a list of StudioWinCountDto normally', () async {
+      final studiosWinCount = studioWinCountMock.createList();
+      when(
+        datasource(),
+      ).thenAnswer(
+        (_) async => studiosWinCount,
+      );
+
+      final result = await usecase();
+
+      expect(result, studiosWinCount);
+    });
   });
 }
