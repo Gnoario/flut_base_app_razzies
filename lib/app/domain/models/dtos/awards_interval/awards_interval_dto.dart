c894207b9ddf9f264383e80f849ac63a93c85018
Tadeu Jenuario <tadeujenuario@hotmail.com>
2024-04-29 14:54:44 -0300

    feat(awards-interval): add dto's

---
+++ lib/app/domain/models/dtos/awards_interval/awards_interval_dto.dart
@@ -0,0 +1,19 @@
+import 'package:flut_base_app_razzies/app/domain/models/dtos/producer_awards_interval/producer_awards_interval_dto.dart';
+
+class AwardsIntervalDto {
+  final List<ProducerAwardsIntervalDto> max;
+  final List<ProducerAwardsIntervalDto> min;
+  AwardsIntervalDto({
+    required this.max,
+    required this.min,
+  });
+
+  factory AwardsIntervalDto.fromJson(Map<String, dynamic> json) {
+    final max = json['max'] as List;
+    final min = json['min'] as List;
+    return AwardsIntervalDto(
+      max: max.map((e) => ProducerAwardsIntervalDto.fromJson(e)).toList(),
+      min: min.map((e) => ProducerAwardsIntervalDto.fromJson(e)).toList(),
+    );
+  }
+}
