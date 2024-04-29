162a49282170f5ec443de73bf4ecc76d0e874ed6
Tadeu Jenuario <tadeujenuario@hotmail.com>
2024-04-29 12:49:14 -0300

    test(studios): should throw a handled exception when json conversion failed

--- test/domain/usecases/get_studions_with_win_count/get_studions_with_win_count_usecase_test.dart
+++ test/domain/usecases/get_studions_with_win_count/get_studions_with_win_count_usecase_test.dart
@@ -1,5 +1,6 @@
 import 'package:flut_base_app_razzies/app/data/datasources/get_studios_with_win_count/get_studios_with_win_count_datasource.dart';
 import 'package:flut_base_app_razzies/app/data/repositories/get_studios_with_win_count/get_studios_with_win_count_imp_repository.dart';
+import 'package:flut_base_app_razzies/app/domain/models/exceptions/exceptions.dart';
 import 'package:flut_base_app_razzies/app/domain/repositories/get_studios_with_win_count/get_studios_with_win_count_repository.dart';
 import 'package:flut_base_app_razzies/app/domain/usecases/get_studios_with_win_count/get_studios_with_win_count_imp_usecase.dart';
 import 'package:flut_base_app_razzies/app/domain/usecases/get_studios_with_win_count/get_studios_with_win_count_usecase.dart';
@@ -52,6 +53,24 @@ void main() {
       final result = await usecase();
 
       expect(result, studiosWinCount);
+    });
+  });
+
+  group('Throws cases', () {
+    test('Should throw a HandledException when json conversion failed',
+        () async {
+      when(
+        datasource(),
+      ).thenAnswer(
+        (_) async => studioWinCountMock.fromList(
+          studioWinCountMock.createStudioWinCountListJsonWithError(),
+        ),
+      );
+
+      expect(
+        () async => await usecase(),
+        throwsA(isA<HandledException>()),
+      );
     });
   });
 }
