38cc4ca07c51f9824fb91321046333c4f7671273
Tadeu Jenuario <tadeujenuario@hotmail.com>
2024-05-01 02:02:25 -0300

    chore(dashboard): export datatables

---
+++ lib/app/ui/pages/dashboard/widgets/datatables.dart
@@ -0,0 +1,4 @@
+export 'list_interval_between_wins_datatable.dart';
+export 'list_movie_winners_by_year_datatable.dart';
+export 'list_multiple_winners_datatable.dart';
+export 'list_top_studios_datatable.dart';
