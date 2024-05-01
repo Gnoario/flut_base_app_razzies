f846b407c2281842b3ff524f0e3c74d0192d13fe
Tadeu Jenuario <tadeujenuario@hotmail.com>
2024-05-01 01:35:09 -0300

    feat(dashboard): create basic components

---
+++ lib/app/ui/components/base/base_page.dart
@@ -0,0 +1,107 @@
+import 'package:flut_base_app_razzies/app/ui/components/text/base_text.dart';
+import 'package:flut_base_app_razzies/core/routes/app_routes.dart';
+import 'package:flut_base_app_razzies/core/theme/theme.dart';
+import 'package:flut_base_app_razzies/shared/utils/utils.dart';
+import 'package:flutter/material.dart';
+
+class BasePage extends StatefulWidget {
+  final Widget child;
+  final String currentRoute;
+  const BasePage({
+    super.key,
+    required this.child,
+    required this.currentRoute,
+  });
+
+  @override
+  State<BasePage> createState() => _BasePageState();
+}
+
+class _BasePageState extends State<BasePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: BaseText(
+          text: 'Frontend Flutter Test',
+          color: context.appColors.brand.primary,
+          fontSize: 18,
+        ),
+        backgroundColor: context.appColors.neutral.grey6,
+      ),
+      body: isResponsiveMode ? _buildColumn() : _buildRow(),
+    );
+  }
+
+  _buildRow() {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: _buildChildren(),
+    );
+  }
+
+  _buildColumn() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: _buildChildren(),
+    );
+  }
+
+  _buildChildren() {
+    return [
+      Expanded(
+        child: Container(
+          padding: context.spacer.all.xs,
+          height: double.infinity,
+          color: context.appColors.brandSecondary.secondary,
+          child: _buildOptions(),
+        ),
+      ),
+      Expanded(
+        flex: 6,
+        child: widget.child,
+      )
+    ];
+  }
+
+  _buildOptions() {
+    return Column(
+      mainAxisAlignment: isResponsiveMode
+          ? MainAxisAlignment.spaceAround
+          : MainAxisAlignment.start,
+      children: [
+        _buildOption(
+          title: 'Dashboard',
+          route: '/',
+        ),
+        SizedBox(height: context.spacer.value.xs),
+        _buildOption(
+          title: 'List',
+          route: '/list-movies',
+        ),
+      ],
+    );
+  }
+
+  _buildOption({required String title, required String route}) {
+    return Expanded(
+      child: ListTile(
+        contentPadding: EdgeInsets.zero,
+        title: BaseText(
+          text: title,
+          color: widget.currentRoute == route
+              ? context.appColors.brandSecondary.greyBlue
+              : null,
+        ),
+        onTap: () {
+          if (widget.currentRoute != route) {
+            AppRoutes.goToRoute(route);
+          }
+        },
+      ),
+    );
+  }
+
+  bool get isResponsiveMode =>
+      ResponsiveHelper.isMobile(context) || ResponsiveHelper.isTablet(context);
+}
---
+++ lib/app/ui/components/components.dart
@@ -0,0 +1,3 @@
+export 'base/base_page.dart';
+export 'text/base_text.dart';
+export 'datatable/datatable.dart';
